16ec585b7c6e758b032ba89c086a378e1de9d26d
rishcodelib <ri.sbh23@gmail.com>
2021-07-29 20:17:31 +0530

    DAY1

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+#  Project 1 ("String Concatenation in Python")
+print("Welcome to the Band name Generator")
+
+city = input("Enter city name?")
+
+petname = input("enter your petname ")
+
+print(" Your Band Name Could be: " + city + " " + petname)
+
